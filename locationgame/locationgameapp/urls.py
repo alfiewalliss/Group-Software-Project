a30a3775ae8279e5c7e220c2509d47e762d8a1c1
hugohewitt123 <52033445+hugohewitt123@users.noreply.github.com>
2022-02-23 11:06:26 +0000

    Add files via upload

--- locationgame/locationgameapp/urls.py
+++ locationgame/locationgameapp/urls.py
@@ -6,4 +6,5 @@ app_name = 'locationgameapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signUp/', views.signUp, name='signUp'),
+    path('login/', views.login, name='login'),
 ]
