7b55e87257e9aca3c9eef899504bb6d2835b9a29
alfiewalliss <alfie.walliss@gmail.com>
2022-02-27 12:09:18 +0000

    Added Logout functionality

--- locationgame/locationgameapp/urls.py
+++ locationgame/locationgameapp/urls.py
@@ -7,8 +7,6 @@ app_name = 'locationgameapp'
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('homePage/', views.homePage, name='homePage'),
-
     path('Game/', views.Game, name='Game'),
 
     path('Settings/', views.Settings, name='Settings'),
