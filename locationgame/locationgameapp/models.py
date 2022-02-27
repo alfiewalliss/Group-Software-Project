dc61fde79e244cff06470eb526486823b3fdbff3
curtisw27 <75444602+curtisw27@users.noreply.github.com>
2022-02-27 23:09:29 +0000

    Added tasks to the map via the database

--- locationgame/locationgameapp/models.py
+++ locationgame/locationgameapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class task(models.Model):
+    taskName = models.CharField(max_length=120)
+    description = models.TextField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
