fcb8a67d9809a1779e4ae2679c34fcc27fc40a8c
curtisw27 <75444602+curtisw27@users.noreply.github.com>
2022-02-28 21:41:09 +0000

    Start of linting + Commenting

--- locationgame/locationgameapp/models.py
+++ locationgame/locationgameapp/models.py
@@ -1,8 +1,17 @@
+'''All database models '''
+
 from django.db import models
 
 # Create your models here.
 
 class task(models.Model):
+
+    '''Model for the task table
+Includes : taskName - Name of the task
+description - Description of the task
+longitude - Longitude coordinate for the location of the task
+latitude - Latitude coordinate for the location of the task'''
+
     taskName = models.CharField(max_length=120)
     description = models.TextField()
     longitude = models.FloatField()
--- locationgame/locationgameapp/views.py
+++ locationgame/locationgameapp/views.py
@@ -1,26 +1,39 @@
+'''Each page in the project'''
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import task
 
-import json 
-
 def index(request):
+    '''Rendering of the index page'''
+
     return render(request, 'locationgameapp/index.html')
 
 def signUp(request):
+    '''Rendering of the signUp page'''
+
     return render(request, 'locationgameapp/signUp.html')
 
+@login_required
 def Game(request):
-    taskList = list(task.objects.order_by('taskName').values()) 
-    taskJson = json.dumps(taskList)  
-    context = {'tasks': taskJson} 
-    return render(request, 'locationgameapp/game.html', context) 
+    '''Rendering of the game page
+    Additionally loads the data from the tasks table
+    This is then used in the game.html js code'''
+
+    #Sort by taskName
+    taskList = list(task.objects.order_by('taskName').values())
+    #Json file preparation
+    taskJson = json.dumps(taskList)
+    context = {'tasks': taskJson}
+    return render(request, 'locationgameapp/game.html', context)
 
 @login_required
 def Settings(request):
+    '''Rendering of the Settings page'''
     return render(request, 'locationgameapp/settings.html')
 
 @login_required
 def Leaderboards(request):
+    '''Rendering of the Leaderboards page'''
     return render(request, 'locationgameapp/Leaderboards.html')
-
