192ce80edb42cfacd65dd738fb4ec145e26ca4e3
Josh <jsf207@exeter.ac.uk>
2022-03-01 17:40:43 +0000

    updated fork

--- locationgame/locationgameapp/admin.py
+++ locationgame/locationgameapp/admin.py
@@ -1,3 +1,10 @@
+'''/admin page -
+Displays the current data inside the databases
+using models provided from models.py'''
+
 from django.contrib import admin
+from .models import task
 
 # Register your models here.
+
+admin.site.register(task)
--- locationgame/locationgameapp/views.py
+++ locationgame/locationgameapp/views.py
@@ -1,23 +1,39 @@
-from django.http import HttpResponse
+'''Each page in the project'''
+import json
+
 from django.contrib.auth.decorators import login_required
-from django.template import loader
-from django.shortcuts import get_object_or_404, render
-from . import views
+from django.shortcuts import render
+from .models import task
+
 def index(request):
+    '''Rendering of the index page'''
+
     return render(request, 'locationgameapp/index.html')
 
 def signUp(request):
+    '''Rendering of the signUp page'''
+
     return render(request, 'locationgameapp/signUp.html')
 
 @login_required
 def Game(request):
-    return render(request,'locationgameapp/game.html')
+    '''Rendering of the game page
+    Additionally loads the data from the tasks table
+    This is then used in the game.html js code'''
+
+    #Sort by taskName
+    taskList = list(task.objects.order_by('taskName').values())
+    #Json file preparation
+    taskJson = json.dumps(taskList)
+    context = {'tasks': taskJson}
+    return render(request, 'locationgameapp/game.html', context)
 
 @login_required
 def Settings(request):
+    '''Rendering of the Settings page'''
     return render(request, 'locationgameapp/settings.html')
 
 @login_required
 def Leaderboards(request):
+    '''Rendering of the Leaderboards page'''
     return render(request, 'locationgameapp/Leaderboards.html')
-
