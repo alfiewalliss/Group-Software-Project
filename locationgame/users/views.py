99ca72ed1e367c2e71c26a3c97ba393242310836
curtisw27 <75444602+curtisw27@users.noreply.github.com>
2022-03-02 15:13:56 +0000

    Further commenting

--- locationgame/users/views.py
+++ locationgame/users/views.py
@@ -1,13 +1,16 @@
+'''Users views'''
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from . import views
 from .forms import UserRegisterFrom
 
 # Create your views here.
+#SignUp data handling
 def register(request):
     if request.method == 'POST':
+        #Recieve user input data
         form = UserRegisterFrom(request.POST)
+        #If the form inputs are correct
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
