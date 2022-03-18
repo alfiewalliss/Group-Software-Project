6a404d1493b51b7b448c22bab56eb214e7551ccf
alfiewalliss <alfie.walliss@gmail.com>
2022-03-18 17:39:53 +0000

    Fix structure

--- technical docs/locationgame/settings.py
+++ technical docs/locationgame/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ### EMAIL HERE 
-EMAIL_HOST_PASSWORD = ### KEY HERE
+EMAIL_HOST_USER = "the.snail.game123@gmail.com"
+EMAIL_HOST_PASSWORD = "kcszmfeuizkevxwj"
