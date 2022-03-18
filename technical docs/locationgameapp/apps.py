ce7665b439032e4a14f7bc3d2f12c8335018ff00
curtisw27 <75444602+curtisw27@users.noreply.github.com>
2022-03-18 21:53:23 +0000

    Profile Pictures

--- technical docs/locationgameapp/apps.py
+++ technical docs/locationgameapp/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class LocationgameappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'locationgameapp'
+
+class usersConfig(AppConfig):
+    def ready(self):
+        import users.signals
