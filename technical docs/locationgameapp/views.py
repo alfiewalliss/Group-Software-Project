d55d92d75bf319b1637182dcee682fb880cda0aa
alfiewalliss <alfie.walliss@gmail.com>
2022-03-19 15:43:40 +0000

    More Game Master

--- technical docs/locationgameapp/views.py
+++ technical docs/locationgameapp/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import task
+from django.contrib import messages
 from .forms import UserCreationForm, profileUpdateForm, userUpdateForm
 
 import json 
@@ -25,18 +26,30 @@ def Leaderboards(request):
 @login_required
 def AddLocations(request):
     if request.method == 'POST':
-        print("hello")
-        print(request.POST.get)
-        task1 = task()
-        task1.taskName = request.POST.get('Name' '')
-        task1.description = request.POST.get('Name' '')
-        task1.longitude = request.POST.get('Longitude' '')
-        task1.latitude = request.POST.get('Latitude' '')
-        task1.save()
+        print(request.POST.get('LocationName'))
+        if request.POST.get('Name') and request.POST.get('Longitude') and request.POST.get('Latitude'):
+            name = request.POST.get('Name')
+            try:
+                longitude = float(request.POST.get('Longitude'))
+                latitude = float(request.POST.get('Latitude'))
+                if type(name) == str and type(longitude) == float and type(latitude) == float:
+                    task1 = task()
+                    task1.description = request.POST.get('Name')
+                    task1.longitude = request.POST.get('Longitude')
+                    task1.latitude = request.POST.get('Latitude')
+                    task1.save()
+                else:
+                    messages.warning(request, f'Error incorect format for new task')
+            except:
+                messages.warning(request, f'Error incorect format for new task')  
+        elif request.POST.get("LocationName"):
+            task.objects.filter(description=request.POST.get("LocationName")).delete()
+        else:
+            messages.warning(request, f'Error incorect format for new task')
     taskList = list(task.objects.order_by('taskName').values()) 
     taskJson = json.dumps(taskList)  
     context = {'tasks': taskJson} 
-    return render(request, 'locationgameapp/addLocations.html', context) 
+    return render(request, 'locationgameapp/addLocations.html', context)
 
 @login_required
 def UpdateProfile(request):
