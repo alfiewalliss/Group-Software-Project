e1d6333ee749237ef7ccaa10db1136254bd684f7
alfiewalliss <alfie.walliss@gmail.com>
2022-03-19 13:02:18 +0000

    Add Locations

---
+++ technical docs/locationgameapp/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import profile
+
+@receiver(post_save, sender=User)
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def saveProfile(sender, instance, **kwargs):
+    instance.profile.save()
--- technical docs/locationgameapp/views.py
+++ technical docs/locationgameapp/views.py
@@ -23,8 +23,20 @@ def Leaderboards(request):
 
 @login_required
 def AddLocations(request):
+    if request.method == 'POST':
+        print("hello")
+        print(request.POST.get)
+        task1 = task()
+        task1.taskName = request.POST.get('Name' '')
+        task1.description = request.POST.get('Name' '')
+        task1.longitude = request.POST.get('Longitude' '')
+        task1.latitude = request.POST.get('Latitude' '')
+        task1.save()
     taskList = list(task.objects.order_by('taskName').values()) 
     taskJson = json.dumps(taskList)  
     context = {'tasks': taskJson} 
     return render(request, 'locationgameapp/addLocations.html', context) 
+
+    
+
 
