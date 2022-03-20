2cb6f4cec72ba8daa5159940efe8af97417876e2
curtisw27 <75444602+curtisw27@users.noreply.github.com>
2022-03-20 23:08:57 +0000

    Creating users issue fixed

--- technical docs/locationgameapp/models.py
+++ technical docs/locationgameapp/models.py
@@ -28,7 +28,7 @@ class profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self,*args, **kwargs):
         super().save()
 
         img = Image.open(self.image.path)
--- technical docs/locationgameapp/signals.py
+++ technical docs/locationgameapp/signals.py
@@ -4,10 +4,10 @@ from django.dispatch import receiver
 from locationgameapp import models
 
 @receiver(post_save, sender=User)
-def createProfile(sender, instance, created, **kwargs):
+def createProfile(sender, instance, created,*args, **kwargs):
     if created:
         models.profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def saveProfile(sender, instance, **kwargs):
-    instance.user.profile.save()
+def saveProfile(sender, instance,*args,  **kwargs):
+    instance.profile.save()
