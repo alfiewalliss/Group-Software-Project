eb62a7fe7235dec033534438f4a93a7560461804
hugohewitt123 <52033445+hugohewitt123@users.noreply.github.com>
2022-03-21 15:17:33 +0000

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import profile, task, pleaderboard
-
-# Register your models here.
-
-admin.site.register(task)
-admin.site.register(profile)
-admin.site.register(pleaderboard)
